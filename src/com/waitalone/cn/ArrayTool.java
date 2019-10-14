0e1c1724d1e513b1ac2a5d6cd7d23f965c53d6f9
独自等待 <xliang@vip.qq.com>
2019-10-14 18:22:13 +0800

    编写api文档

--- src/com/waitalone/cn/ArrayTool.java
+++ src/com/waitalone/cn/ArrayTool.java
@@ -6,7 +6,12 @@ package com.waitalone.cn;
  * @Time: 2019-10-14 17:25
  * @Blog: https://www.waitalone.cn
  */
-class ArrayTool {
+
+/*这是一个数组操作的工具类，可以获取最值
+ * @author 张三
+ * @version v1.0
+ * */
+public class ArrayTool {
     /*静态的应用
      * 每一个应用程序中都有共性的功能，可以将这些功能进行抽取，以便于复用。
      * 虽然可以通过建议ArrayTool对象使用这些工具方法，对数组进行操作，但是发现以下问题
@@ -20,9 +25,15 @@ class ArrayTool {
      * 为了更为严谨，强制让该类不能建立对象，可以通过将构造函数私有化完成
      * */
     private ArrayTool() {
-        //防止对象调用
+        //空参数的构造函数，加上private后可以防止对象调用
     }
 
+    /**
+     * 获取一个整型数组中的最大值
+     *
+     * @param arr 接收一个int类型的数组
+     * @return 会返回数组中的最大值
+     */
     public static int getMax(int[] arr) {
         int max = 0;
         for (int x = 1; x < arr.length; x++) {
@@ -33,6 +44,12 @@ class ArrayTool {
         return arr[max];
     }
 
+    /**
+     * 获取一个整型数组中的最小值
+     *
+     * @param arr 接收一个int类型的数组
+     * @return 会返回数组中的最小值
+     */
     public static int getMin(int[] arr) {
         int min = 0;
         for (int x = 1; x < arr.length; x++) {
